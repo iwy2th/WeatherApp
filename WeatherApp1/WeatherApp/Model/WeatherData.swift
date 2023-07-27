a3a90203c8776c40c8a5ab2fa634843d78df5b8b
iwy2th <huyloind03@gmail.com>
2023-07-27 10:35:32 +0700

    JSON decoding

---
+++ WeatherApp1/WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by Iwy2th on 27/07/2023.
+//
+
+import Foundation
+struct WeatherData: Codable {
+  let name: String
+  let main: Main
+  let weather: [Weather]
+}
+
+struct Main: Codable {
+  let temp: Int
+  let feels_like: Double
+}
+struct Weather: Codable {
+  let main: String
+}
--- WeatherApp1/WeatherApp/Model/WeatherManager.swift
+++ WeatherApp1/WeatherApp/Model/WeatherManager.swift
@@ -19,16 +19,24 @@ struct WeatherManager {
     guard let url = URL(string: urlString) else { return }
     let session = URLSession(configuration: .default)
     let task = session.dataTask(with: url) { data, response, error in
-      if error != nil {
-        return
-      }
-      if let safeData = data {
-        let dataString = String(data: safeData, encoding: .utf8)
-        print(dataString!)
+      if error != nil { return }
+      if let data {
+        self.parseJson(weatherData: data)
       }
     }
     task.resume()
-    
+  }
+  // ParseJson
+  func parseJson(weatherData data: Data) {
+    let decoder = JSONDecoder()
+    do {
+       let decodedData = try decoder.decode(WeatherData.self, from: data)
+      print(decodedData.main.temp)
+      print(decodedData.main.feels_like)
+      print(decodedData.weather[0].main)
+    } catch {
+      print(error)
+    }
   }
   
 }
